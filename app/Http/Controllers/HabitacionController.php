190bb20d6f03f6d099baea1a5a931a97d3daead5
Sol-star12 <sorojasc@ucvvirtual.edu.pe>
2024-11-27 00:06:04 -0500

    Update HabitacionController.php

--- app/Http/Controllers/HabitacionController.php
+++ app/Http/Controllers/HabitacionController.php
@@ -5,31 +5,48 @@ use Illuminate\Http\Request;
 
 class HabitacionController extends Controller
 {
+     /**
+     * Muestra una lista de habitaciones disponibles.
+     */
     public function index()
     {
         //
     }
-    
+     /**
+     * Muestra el formulario para registrar una nueva habitación.
+     */
     public function create()
     {
         //
     }
     public function store(Request $request)
     {
+         /**
+     * Muestra los detalles de una habitación específica.
+     */
         //
     }
     public function show(string $id)
     {
         //
     }
+     /**
+     * Muestra el formulario para editar una habitación existente.
+     */
     public function edit(string $id)
     {
+         /**
+     * Actualiza la información de una habitación específica.
+     */
         //
     }
     public function update(Request $request, string $id)
     {
         //
     }
+    /**
+     * Elimina una habitación específica de la base de datos.
+     */
     public function destroy(string $id)
     {
         //
